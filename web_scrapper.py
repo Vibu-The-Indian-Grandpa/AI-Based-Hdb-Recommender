4811b9273da21cb4365dfb561b7a8a4b3ad4f6c6
vibu vignesh <vibumy@gmail.com>
2024-07-20 18:22:42 +0800

    adding no sandbox to option

--- web_scrapper.py
+++ web_scrapper.py
@@ -15,6 +15,8 @@ import pandas as pd
 import numpy as np
 # create driver object
 options=webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
